8fa9c1129c16871657e78396f7bc4a48350e142a
Duy <hugoquan9@gmail.com>
2025-12-01 17:41:28 +0700

    fix: add check token route

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
   return (
     <div>
       <button
-        onClick={() => navigate("/")}
+        onClick={() => navigate("/home")}
         className="w-full inline-flex items-center justify-center px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-500"
       >
         Sign in
