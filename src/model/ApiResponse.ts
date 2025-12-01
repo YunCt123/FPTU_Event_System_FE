55d5ca0993ab3aec6f8d1ea4531270271e55d797
YunCt123 <yunct1234@gmail.com>
2025-12-01 15:11:04 +0700

    first  commit

---
+++ src/model/ApiResponse.ts
@@ -0,0 +1,15 @@
+export interface ApiResponse<T> {
+    success: boolean;
+    data: T;
+    message?: string;
+  }
+  export interface ErrorResponse {
+    success?: boolean;
+    data: {
+      message?: string,
+    }
+    error?: [
+      message?: string,
+      field?: string,
+    ]
+  }
---
+++ src/utils/theme.ts
@@ -0,0 +1,22 @@
+// export const theme = {
+//   colors: {
+//     primary: {
+//       main: '#F36F21', 
+//       tailwind: 'bg-orange-500',
+//     },
+//     secondary: {
+//       main: '#004070', 
+//       tailwind: 'bg-blue-900',
+//     },
+//     neutral: {
+//       main: '#F0F2F5', 
+//       tailwind: 'bg-gray-100',
+//     },
+//     text: {
+//       main: '#333333',
+//       tailwind: 'text-gray-800',
+//     },
+//   },
+// } as const;
+
+// export type Theme = typeof theme;
